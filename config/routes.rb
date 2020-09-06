bc689a72367a268eabdf1b54da7502e28192d7f4
EC2 Default User <ec2-user@ip-172-31-30-204.ec2.internal>
2020-09-06 01:26:43 +0000

    [Add]トップ、一覧・新規投稿、詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/' => 'books#top'
+  get 'books/index' => 'books#index'
+  post 'books/create' => 'books#create'
+  get 'books/edit'
+  
+  get 'books/:id' => 'books#show', as: 'books'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
